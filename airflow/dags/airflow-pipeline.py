0b9d5f5633f488b6ad9524e00f251ae05e5da0bb
Muskan Raisinghani <raisinghani.m@northeastern.edu>
2024-03-29 12:41:17 -0400

    airflow execution completed

--- airflow/dags/airflow-pipeline.py
+++ airflow/dags/airflow-pipeline.py
@@ -1,10 +1,9 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from airflow.operators.bash import BashOperator
 # from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
-from extraction.pdf_extraction import pdf_to_xml_using_grobid, xml_data_to_csv
-from validation.corrections.correction_pdf_content import content_validate_data, content_clean_csv_generate
-from validation.corrections.correction_pdf_metadata import metadata_validate_data, metadata_clean_csv_generate
+from extraction.pdf_extraction import process_pdf_content
+from validation.corrections.correction_pdf_content import content_clean_csv_generate
+# from validation.corrections.correction_pdf_metadata import metadata_validate_data, metadata_clean_csv_generate
 from load.load_data_to_snowflake import load_data_to_snowflake_func
 import configparser
 
@@ -14,10 +13,24 @@ dag= DAG(
     schedule_interval=None,
 )
 
-config = configparser.ConfigParser()
-config.read('configuration.properties')
+def extract_data(**kwargs):
+    s3_uri = kwargs['dag_run'].conf.get("s3_uri")
+    csv_s3_uri = process_pdf_content(s3_uri)
+    kwargs['ti'].xcom_push(key='raw_csv', value=csv_s3_uri)
+    
+def validate_data(**kwargs):
+    pulled_value = kwargs['ti'].xcom_pull(dag_id='trigger_pipeline', task_ids='Data_Extraction_task', key='raw_csv')
+    file_name = content_clean_csv_generate(pulled_value)
+    kwargs['ti'].xcom_push(key='clean_csv', value=file_name)
+    
+def load_data(**kwargs):
+    pulled_value = kwargs['ti'].xcom_pull(dag_id='trigger_pipeline', task_ids='Content_Validation_task', key='clean_csv')
+    load_data_to_snowflake_func(pulled_value)
 
-file_format_name = config['SNOWFLAKE']['file_format_name']
+config = configparser.ConfigParser()
+config.read('dags/configuration.properties')
+
+file_format_name = config['SNOWFLAKE']['pdf_data_file_format']
 content_table_name = config['SNOWFLAKE']['content_table_name']
 content_stage_name = config['SNOWFLAKE']['content_stage_name']
 metadata_table_name = config['SNOWFLAKE']['metadata_table_name']
@@ -27,37 +40,17 @@ with dag:
     
     extraction_task = PythonOperator(
         task_id='Data_Extraction_task',
-        python_callable=pdf_to_xml_using_grobid
+        python_callable=extract_data
     )
     
     content_validation = PythonOperator(
         task_id='Content_Validation_task',
-        python_callable=content_validate_data
+        python_callable=validate_data
     )
     
-    content_clean_csv =PythonOperator(
-        task_id='content_csv_generation_task',
-        python_callable=content_clean_csv_generate
-    )
-    
-    metadata_validation = PythonOperator(
-        task_id='metadata_Validation_task',
-        python_callable=metadata_validate_data
-    )
-    
-    metadata_clean_csv =PythonOperator(
-        task_id='metadata_csv_generation_task',
-        python_callable=metadata_clean_csv_generate
-    )
-    
-    snowflake_data_load_content = PythonOperator(
-        task_id='snowflake_load_content_task',
-        python_callable=load_data_to_snowflake_func(content_table_name, content_stage_name,  file_format_name)
-    )
-    
-    snowflake_data_load_metadata = PythonOperator(
-        task_id='snowflake_load_metadata_task',
-        python_callable=load_data_to_snowflake_func('metadata_table_name','metadata_stage_name','file_format_name')
+    load_data_snowflake =PythonOperator(
+        task_id='Load_data_snowflake',
+        python_callable=load_data
     )
 
-    extraction_task >> content_validation >> content_clean_csv >> metadata_validation >> metadata_clean_csv >> snowflake_data_load_content >> snowflake_data_load_metadata
+    extraction_task >> content_validation >> load_data_snowflake
